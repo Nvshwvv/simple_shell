2fbfa23aa6a44c26f0d5d6e7840ddfbc005d47dd
monia809 <maa0192001@hotmail.com>
2023-11-13 08:03:07 -0800

    fixed

--- Deal_with_strings.c
+++ Deal_with_strings.c
@@ -1,69 +1,69 @@
 #include "Shell.h"
-/*
-* _strlen - function that return the length of string 
-* @n: the string will be chacked 
-* Return: int number 
+/**
+ * _strlen - function that return the length of string
+ * @n: the string will be chacked
+ * Return: int number
 */
 int _strlen(char *n)
 {
-int l = 0;
+	int l = 0;
 
-if (!n)
-    return (0);
-while (*n++)
-    l++;
-return (l);
+	if (!n)
+		return (0);
+	while (*n++)
+		l++;
+	return (l);
 }
 
-/*
-* _strcmp - function copare two strings 
-* @n: first string
-* @a: second string
-* Return: negative or positive or zero 
+/**
+ * _strcmp - function copare two strings
+ * @n: first string
+ * @a: second string
+ * Return: negative or positive or zero
 */
 int _strcmp(char *n, char *a)
 {
-while (*n && *a)
-{
-    if (*n != *a)
-        return (*n - *a);
-    n++;
-    a++;
-}
-if (*n == *a)
-    return (0);
-else
-return (*n < *a ? -1 : 1);
+	while (*n && *a)
+	{
+		if (*n != *a)
+			return (*n - *a);
+		n++;
+		a++;
+	}
+	if (*n == *a)
+		return (0);
+	else
+		return (*n < *a ? -1 : 1);
 }
 
-/* 
-*starts_with - function that check if string start with another string 
-* @needle: substring to find
-* @haystack: string to search 
-* Return: NULL or address of next char of haystack
+/**
+ * starts_with - function that check if string start with another string
+ * @needle: substring to find
+ * @haystack: string to search
+ * Return: NULL or address of next char of haystack
 */
 char *starts_with(const char *haystack, const char *needle)
 {
-while (*needle)
-    if  (*needle++ != *haystack++)
-        return (NULL);
-return ((char *)haystack);
-}4
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
 
-/*
-* _strcat - function that concates two strings 
-* @destination: the destination buffer 
-* @source: the source buffer 
-* Return: pointer to destination buffer
+/**
+ * _strcat - function that concates two strings
+ * @destination: the destination buffer
+ * @source: the source buffer
+ * Return: pointer to destination buffer
 */
 char *_strcat(char *destination, char *source)
 {
-char *ret = destination;
+	char *ret = destination;
 
-while (*destination)
-    destination++;
-while (*source)
-    *destination++ = *source++;
-*destination = *source;
-return (ret);
+	while (*destination)
+		destination++;
+	while (*source)
+		*destination++ = *source++;
+	*destination = *source;
+	return (ret);
 }
