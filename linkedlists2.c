ab011e7111b2df87806fe56500c0141890578017
monia809 <maa0192001@hotmail.com>
2023-11-14 13:35:24 -0800

    fixed

--- linkedlists2.c
+++ linkedlists2.c
@@ -1,90 +1,91 @@
 #include "Shell.h"
-/
-* list_to_string - function that return array of strings
-* @head: ptr to first node
-* Return: an arrays of string
+/**
+ * list_to_string - function that return array of strings
+ * @head: ptr to first node
+ * Return: an arrays of string
 */
 char **list_to_string(list_t *head)
 {
-    size_t i = list_len(head), p;
-    list_t *node = head;
-    char *str;
-    char **strs;
+	size_t i = list_len(head), p;
+	list_t *node = head;
+	char *str;
+	char **strs;
 
-    if (!head || !i)
-        return (NULL);
-    for (i = 0; node; node = node->next, i++)
-    {
-        str = malloc(_strlen(node->str) + 1);
-        if (!str)
-        {
-            for (p = 0; p < i; p++)
-                free(strs[p]);
-            free(strs);
-            return (NULL);
-        }
-        str = _strcpy(str, node->str);
-        strs[i] = str;
-    }
-    strs[i] = NULL;
-    return (strs);
+	if (!head || !i)
+		return (NULL);
+	for (i = 0; node; node = node->next, i++)
+	{
+		str = malloc(_strlen(node->str) + 1);
+		if (!str)
+		{
+			for (p = 0; p < i; p++)
+				free(strs[p]);
+			free(strs);
+			return (NULL);
+		}
+		str = _strcpy(str, node->str);
+		strs[i] = str;
+	}
+	strs[i] = NULL;
+	return (strs);
 }
-/
-* print_list - prints all elements of linked lists
-* @n: pointer
-*Return: size of the list
+/**
+ * print_list - prints all elements of linked lists
+ * @n: pointer
+ * Return: size of the list
 */
 size_t print_list(const list_t *n)
 {
-    size_t l = 0;
+	size_t l = 0;
 
-    while (n)
-    {
-        _puts(convert_number(n->num, 10, 0));
-        _putchar(':');
-        _putchar(' ');
-        _puts(n->str ? n->str : "(nill)");
-        _puts("\n");
-        n = n->next;
-        l++;
-    }
-    return (l);
-/
-* node_starts_with - func return the start
-* @node: pointer to the head
-* @prefix: string to match
-* @n: the next charachter
-* Return:match node or NULL
+	while (n)
+	{
+		_puts(convert_number(n->num, 10, 0));
+		_putchar(':');
+		_putchar(' ');
+		_puts(n->str ? n->str : "(nill)");
+		_puts("\n");
+		n = n->next;
+		l++;
+	}
+	return (l);
+}
+/**
+ * node_starts_with - func return the start
+ * @node: pointer to the head
+ * @prefix: string to match
+ * @n: the next charachter
+ * Return:match node or NULL
 */
 list_t *node_starts_with(list_t *node, char *prefix, char n)
 {
-    char *l = NULL;
+	char *l = NULL;
 
-    while (node)
-    {
-        l = starts_with(node->str, prefix);
-        if (l && ((n == -1) || (*l == n)))
-            return (node);
-        node = node->next;
-    }
-    return (NULL);
+	while (node)
+	{
+		l = starts_with(node->str, prefix);
+		if (l && ((n == -1) || (*l == n)))
+			return (node);
+		node = node->next;
+	}
+	return (NULL);
 }
-/
-* get_node_index - function that gets the index of node
-* @head: pointer to the head
-* @node: pointer to the node
-* Return: index or -1 depened on the situation
+/**
+ * get_node_index - function that gets the index of nod
+ * @head: pointer to the head
+ * @node: pointer to the node
+ * Return: index or -1 depened on the situation
 */
 size_t get_node_index(list_t *head, list_t *node)
 {
-    size_t l = 0;
+	size_t l = 0;
 
-    while (head)
-    {
-        if (head == node)
-            return (l);
-        head = head->next;
-        l++;
-    }
-    return (-1);
+	while (head)
+	{
+		if (head == node)
+			return (l);
+		head = head->next;
+		l++;
+	}
+	return (-1);
 }
