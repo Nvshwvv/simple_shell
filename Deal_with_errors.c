aa53e4c542517910e84d083e854c5a398231b7ae
monia809 <maa0192001@hotmail.com>
2023-11-14 08:45:53 -0800

    fixed

--- Deal_with_errors.c
+++ Deal_with_errors.c
@@ -1,40 +1,40 @@
 #include "Shell.h"
-/
+/**
  * _eputs - functon print an input string
  * @str: the string will be printed
  * Return: nothing
 */
 void _eputs(char *str)
 {
-    int l = 0;
+	int l = 0;
 
-    if (!str)
-        return;
-    while (str[l] != '\0')
-    {
-        _eputchar(str[l]);
-        l++;
-    }
+	if (!str)
+		return;
+	while (str[l] != '\0')
+	{
+		_eputchar(str[l]);
+		l++;
+	}
 }
 
-/
+/**
  * _eputchar - function print input string
  * @n: te character will be printed
  * Return: -1 or 0 depend on situation
 */
 int _eputchar(char n)
 {
-    static int i;
-    static char buf[WRITE_BUF_SIZE];
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
 
-    if (n == BUF_FLUSH  i >= WRITE_BUF_SIZE)
-    {
-        write(2, buf, i);
-        i = 0;
-    }
-    if (n != BUF_FLUSH)
-        buf[i++] = n;
-        return (1);
+	if (n == BUF_FLUSH  i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (n != BUF_FLUSH)
+		buf[i++] = n;
+	return (1);
 }
 /**
  * _putfd - function that writes char to given fd
@@ -44,17 +44,17 @@ int _eputchar(char n)
 */
 int _putfd(char ch, int fd)
 {
-    static int i;
-    static char buf[WRITE_BUF_SIZE];
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
 
-    if (ch == BUF_FLUSH  i >= WRITE_BUF_SIZE)
-        {
-            write(fd, buf, i);
-            i = 0;
-        }
-        if (ch != BUF_FLUSH)
-            buf[i++] = ch;
-            return (1);
+	if (ch == BUF_FLUSH  i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (ch != BUF_FLUSH)
+		buf[i++] = ch;
+	return (1);
 }
 /**
  * _putsfd - function that print an input string
@@ -64,13 +64,13 @@ int _putfd(char ch, int fd)
 */
 int _putsfd(char *strs, int fd)
 {
-    int o = 0;
+	int o = 0;
 
-    if (!=strs)
-        return (0);
-    while (*strs)
-{
-    o += _putfd(*strs++, fd);
-}
-    return (o)
+	if (!strs)
+		return (0);
+	while (*strs)
+	{
+		o += _putfd(*strs++, fd);
+	}
+	return (o)
 }
