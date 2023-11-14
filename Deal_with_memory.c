07e92c6f80c4159d29a9576f80d58ffea7b5f8dc
monia809 <maa0192001@hotmail.com>
2023-11-14 08:33:54 -0800

    fixed

--- Deal_with_memory.c
+++ Deal_with_memory.c
@@ -1,58 +1,60 @@
 #include "Shell.h"
-/
-* *_memset - function that fills the memory with a constant bytes
-* @s: pointer to memory area
-* @b: num of bytes 
-* @n: amount of bytes filled
+
+/**
+ * *_memset - function that fills the memory with a constant bytes
+ * @s: pointer to memory area
+ * @b: num of bytes
+ * @n: amount of bytes filled
+ * Return: s
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int w;
+	unsigned int w;
 
-    for (w = 0; w < n; w++)
-        s[i] = b;
-    return (s);
+	for (w = 0; w < n; w++)
+		s[i] = b;
+	return (s);
 }
-/
-* ffree - function that frees string of string
-* @ss: string of string
+/**
+ * ffree - function that frees string of string
+ * @ss: string of string
 */
 void ffree(char ss)
 {
-    char **e = ss;
+	char **e = ss;
 
-    if (!ss)
-        return;
-    while (*ss)
-        free (*ss++);
-    free (e);
+	if (!ss)
+		return;
+	while (*ss)
+		free(*ss++);
+	free(e);
 }
 
-/
-* _realloc - reallocat blocks in a memory 
-* @ptr: pointer 
-* @nwsize: bytees size of new block
-* @oldsize: bytes size of the old block
-* Return: pointer
+/**
+ * _realloc - reallocat blocks in a memory
+ * @ptr: pointer
+ * @nwsize: bytees size of new block
+ * @oldsize: bytes size of the old block
+ * Return: pointer
 */
 void *_realloc(void *ptr, unsigned int oldsize, unsigned int nwsize)
 {
-    char *n;
+	char *n;
 
-    if (!ptr)
-        return (malloc(nwsize));
-    if (!nwsize)
-        return (free(ptr), NULL);
-    if (nwsize == oldsize)
-        return (ptr);
+	if (!ptr)
+		return (malloc(nwsize));
+	if (!nwsize)
+		return (free(ptr), NULL);
+	if (nwsize == oldsize)
+		return (ptr);
 
-    n = malloc(nwsize);
-    if (!n)
-        return (NULL);
+	n = malloc(nwsize);
+	if (!n)
+		return (NULL);
 
-    oldsize = oldsize < nwsize ? oldsize : nwsize;
-    while (oldsize--)
-        n[oldsize] = ((char *)ptr) [oldsize];
-    free(ptr);
-    return (n);
+	oldsize = oldsize < nwsize ? oldsize : nwsize;
+	while (oldsize--)
+		n[oldsize] = ((char *)ptr)[oldsize];
+	free(ptr);
+	return (n);
 }
--- Deal_with_memory2.c
+++ Deal_with_memory2.c
@@ -1,16 +1,16 @@
 #include "Shell.h"
 /**
-* bfree - function that frees a pointer 
-* @ptrs: address of the pointer will be freed
-* Return: 1 if freed, 0 if otherwise
+ * bfree - function that frees a pointer
+ * @ptrs: address of the pointer will be freed
+ * Return: 1 if freed, 0 if otherwise
 */
 int bfree(void **ptrs)
 {
-    if (ptrs && *ptrs)
-    {
-        free(*ptrs);
-        *ptrs = NULL;
-        return (1);
-    }
-    return (0);
+	if (ptrs && *ptrs)
+	{
+		free(*ptrs);
+		*ptrs = NULL;
+		return (1);
+	}
+	return (0);
 }
