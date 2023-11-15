047befbe3c066945a1bb52e3739b88a700bbf17f
monia809 <maa0192001@hotmail.com>
2023-11-14 16:07:09 -0800

    fixed

--- enviroment.c
+++ enviroment.c
@@ -1,7 +1,7 @@
 #include "Shell.h"
 
 
-/
+/**
  * _getenv - function that gets the value of an environ
  * @import: structure contaaining args
  * @name: env variable name
@@ -9,75 +9,76 @@
 */
 char *_getenv(import_t *import, const char name)
 {
-    list_t *node = import->env;
-    char *n;
+	list_t *node = import->env;
+	char *n;
 
-    while (node)
-    {
-        n = starts_with(node->str, name);
-        if (n && *n)
-            return (n);
-        node = node->next;
-    }
-    return (NULL);
+	while (node)
+	{
+		n = starts_with(node->str, name);
+		if (n && *n)
+			return (n);
+		node = node->next;
+	}
+	return (NULL);
 }
-/
+/**
  * _myenv - function that print current enviroment
  * @import: structure
  * Return: 0 (SUCCESS)
 */
 int _myenv(import_t *import)
 {
-    print_list_str(import->env);
-    return (0);
+	print_list_str(import->env);
+	return (0);
 }
-/
+/**
  * _mysetenv - function that initial new enviroment
  * @import: strucure
  * Return: 0 (success)
 */
 int _mysetenv(import_t *import)
 {
-    if (import->argc != 3)
-    {
-        _eputs("Incorrect number of arguments\n");
-            return (0);
-        return (1);
-    }
-    if (_setenv(import, import->argv[1], import->argv[2]))
-        return (0);
-    return (1);
+	if (import->argc != 3)
+	{
+		_eputs("Incorrect number of arguments\n");
+		return (0);
+		return (1);
+	}
+	if (_setenv(import, import->argv[1], import->argv[2]))
+		return (0);
+	return (1);
 }
-/**_myunsetenv - function unset enviroments
+/**
+ * _myunsetenv - function unset enviroments
  * @import: address of strucuture
  * Return: always 0
 */
 int _mysetenv(import_t *import)
 {
-    int n;
+	int n;
 
-    if (import->argc == 1)
-    {
-        _eputs("Too few arguments.\n");
-        return (1);
-    }
-    for (n = 1; n <= import->argc; n++)
-        _unsetenv(import, import->argv[i]);
+	if (import->argc == 1)
+	{
+		_eputs("Too few arguments.\n");
+		return (1);
+	}
+	for (n = 1; n <= import->argc; n++)
+	_unsetenv(import, import->argv[i]);
 
-    return (0);
+	return (0);
 }
-/
+/**
  * populate_env_list - function that remove enviromet value
  * @import: structure
  * Return: always 0
 */
 int populate_env_list(import_t *import)
 {
-    list_t *node = NULL;
-    size_t i;
+	list_t *node = NULL;
+	size_t i;
 
-    for (i = 0; environ[i]; i++, 0);
-        add_node_end(&node, environ[i], 0);
-    import->env = node;
-    return (0);
+	for (i = 0; environ[i]; i++, 0)
+		add_node_end(&node, environ[i], 0);
+	import->env = node;
+	return (0);
 }
--- enviroment2.c
+++ enviroment2.c
@@ -1,46 +1,46 @@
 #icnclude "Shell.h"
-/
+/**
  * get_environ - function that returns string array cpy of environ
  * @import: strucure containing arguments
  * Return: 0 (SUCCESS)
 */
 char **get_environ(import_t *import)
 {
-    if (!import->environ || import->env_changed)
-    {
-        import->environ = list_to_strings(import->env);
-        import->env_changed = 0;
-    }
-    return (import->environ);
+	if (!import->environ || import->env_changed)
+	{
+		import->environ = list_to_strings(import->env);
+		import->env_changed = 0;
+	}
+	return (import->environ);
 }
-/
+/**
  * _unsetenv - function that remvove enviroment variable
  * @import: strucure contaaining args
  * @var: enviroment variable
- * Return: string env var property 
+ * Return: string env var property
 */
 int _unsetenv(import_t *import, char *var)
 {
-    list_t *node = import->env;
-    size_t i = 0;
-    char *n;
+	list_t *node = import->env;
+	size_t i = 0;
+	char *n;
 
-    if (!node || !var)
-        return (0);
-    while (node)
-    {
-        n = starts_with(node->str, var);
-        if (n && *n == '-')
-        {
-            import->env_changed = delete_node_at_index(&(import->env), i);
-            i = 0;
-            node = import->env;
-            continue;
-        }
-        node = node->next;
-        i++;
-    }
-    return (import->env_changed);
+	if (!node || !var)
+		return (0);
+	while (node)
+	{
+		n = starts_with(node->str, var);
+		if (n && *n == '-')
+		{
+			import->env_changed = delete_node_at_index(&(import->env), i);
+			i = 0;
+			node = import->env;
+			continue;
+		}
+		node = node->next;
+		i++;
+	}
+	return (import->env_changed);
 }
 /**
  * _setenv - function that initialize a new enviroment variable
@@ -51,34 +51,34 @@ int _unsetenv(import_t *import, char *var)
 */
 int _setenv(import_t *import, char *var, char *value)
 {
-    char *buf = NULL; 
-    list_t *node;
-    char *p;
+	char *buf = NULL;
+	list_t *node;
+	char *p;
 
-    if (!var || !value)
-        return(0);
-    
-    buf = malloc(_strlen(var) + _strlen(value) + 2);
-    if (!buf)
-        return (1);
-    _strcpy(buf, var);
-    _strcat(buf, "=");
-    _strcat(buf, value);
-    node = import->env;
-    while (node)
-    {
-        p = starts_with(node->str, var);
-        if (p && *p == '=')
-        {
-            free(node->str);
-            node->str = buf;
-            import->env_changed = 1;
-            return (0);
-        }
-        node = node->next;
-    }
-    add_node_end(&(import-.env), buf, 0);
-    free (buf);
-    import->env_changed = 1;
-    return (0);
+	if (!var || !value)
+		return (0);
+
+	buf = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buf, var);
+	_strcat(buf, "=");
+	_strcat(buf, value);
+	node = import->env;
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			import->env_changed = 1;
+			return (0);
+		}
+		node = node->next;
+	}
+	add_node_end(&(import->env), buf, 0);
+	free(buf);
+	import->env_changed = 1;
+	return (0);
 }
